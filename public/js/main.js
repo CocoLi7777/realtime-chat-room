e36ef039ed67c5ec0f9e2d92c9d587d533e8a271
Coco <coco.li7777@gmail.com>
2020-06-18 16:26:55 +1000

    add join and leave room, fix the UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const chatForm = document.getElementById('chat-form');
 
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
@@ -9,6 +11,11 @@ const { username, room } = Qs.parse(location.search, {
 const socket = io();
 
 socket.emit('joinRoom', { username, room });
+
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 socket.on('message', (message) => {
   outMessage(message);
@@ -34,4 +41,14 @@ function outMessage(message) {
   <p class="text">${message.text}</p>
   `;
   document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join('')}
+  `;
 }
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,20 +20,43 @@ const botName = 'ChatRoom Bot';
 
 io.on('connection', (socket) => {
   socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
     socket.emit('message', formatMessage(botName, 'Welcome to ChatRoom!'));
 
-    socket.broadcast.emit(
-      'message',
-      formatMessage(botName, 'A user has joined the chat')
-    );
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
   });
 
   socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('user', msg));
+    const user = getCurrentUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('message', formatMessage(user.username, msg));
+    }
   });
 });
 
